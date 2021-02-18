609974fec4c2396e512a39d1176ecbed5cf4ad1c
Lucius <39566289+TCT9@users.noreply.github.com>
2021-02-18 20:02:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function print(string_value) {
     process.stdout.write(string_value);
 }
 
-//compara dois números (n1 e n2) e a diferença entre eles for maior do que
-//delta (0.000_000_5), consideremos estes números iguais!
+//compara dois números n1 e n2 (```Math.abs(n1 - n2)```) e se a diferença entre eles for maior do que um 
+//delta (0.000_000_5), consideraremos estes números iguais
 function numberIsEqual(n1, n2, delta = 0.000_000_5){
     let dif_abs = Math.abs(n1 - n2);
     return dif_abs <= delta ? true : false;
